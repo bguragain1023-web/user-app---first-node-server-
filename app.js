a68a3c99c67b21d8c327c10d1e8ad098680612a7
Brazesh Guragain <b.guragain1023@gmail.com>
2026-05-09 20:03:22 +1000

    created controller for login and register pages

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import { dir } from 'console';
 import express from 'express';
 import path from 'path';
 
@@ -6,15 +7,44 @@ const PORT = 8000;
 const __dirname = path.resolve();
 
 
+
+// serve static files fronm public folder
+app.use(express.static(__dirname + "/public"));
+
+
+
+
 // app.get('/', (req, res, next) => {
 
 // console.log("we got the request");
 // res.send("<h1>hello world</h1><p>@djbfhasff hjfsfhb i am brazesg</p>");
 // });
 
+
+
+// HomePAge controller
 app.get("/", (req, res) => {
 res.sendFile(__dirname + "/src/html/index.html");
 });
+
+// user registration controller
+app.get("/register", (req, res) => {
+res.sendFile(__dirname + "/src/html/register.html");
+});
+
+
+//user login controller
+app.get("/login", (req, res) => {
+res.sendFile(__dirname + "/src/html/login.html");
+});
+
+// app.get("/user", (req, res) => {
+// res.json({
+//     name: "brazesh",
+//     age: 23,
+//     email: "brazesh@example.com"  
+// });
+// });
 
 
 app.listen(PORT, (error) => {
