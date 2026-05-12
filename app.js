3524e201043110074372ef5aed3df7c447e7e8e3
Brazesh Guragain <b.guragain1023@gmail.com>
2026-05-12 14:40:23 +1000

    created csv file and writing data in scv file

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-import { dir } from 'console';
+
 import express from 'express';
 import path from 'path';
+import fs from 'fs';
 
 const app = express();
 const PORT = 8000;
@@ -10,15 +11,10 @@ const __dirname = path.resolve();
 
 // serve static files fronm public folder
 app.use(express.static(__dirname + "/public"));
+app.use(express.urlencoded({extended:true})) // to parse the form data and make it available in req.body
 
 
 
-
-// app.get('/', (req, res, next) => {
-
-// console.log("we got the request");
-// res.send("<h1>hello world</h1><p>@djbfhasff hjfsfhb i am brazesg</p>");
-// });
 
 
 
@@ -28,7 +24,25 @@ res.sendFile(__dirname + "/src/html/index.html");
 });
 
 // user registration controller
+const fileName = "userList.csv";
 app.get("/register", (req, res) => {
+    console.log(req.query);
+    console.log("data recieved")
+    
+res.sendFile(__dirname + "/src/html/register.html");
+});
+app.post("/register", (req, res) => {
+  
+    const {name, email, password} = req.body;
+
+    const str = `${name}, ${email}, ${password} \n`;
+
+    // create file and wrote data to it
+fs.appendFile(fileName, str, (error) =>{
+    error ? console.log(error) : console.log("data written successfully")
+})
+
+    
 res.sendFile(__dirname + "/src/html/register.html");
 });
 
